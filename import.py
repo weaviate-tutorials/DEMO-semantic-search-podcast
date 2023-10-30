33aa8c702884a97b71d4710eb9391f337876fddd
Madhav Kumar <lakshy1523@gmail.com>
2023-10-31 02:25:29 +0530

    Added progress bar to new weaviate client

--- import.py
+++ import.py
@@ -2,13 +2,15 @@ import weaviate
 from weaviate import Config
 import weaviate.classes as wvc
 import json
+import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from tqdm import tqdm
+import helper
 
 # Starting up the weaviate client
 client = weaviate.Client(
     "http://localhost:8080",
-    additional_config=Config(grpc_port_experimental=50051),
-    # timeout_config = (100000, 100000)
+    additional_config=Config(grpc_port_experimental=50051)
 )
 
 print(client.collection.delete("Podcast"))
@@ -32,7 +34,7 @@ client.collection.create(
 # Checking if the collection is created successfully
 print(client.collection.exists("Podcast"))
 
-with open("./data/podcast_ds.json", 'r', encoding="utf-8") as f:
+with open("./data/podcast_ds.json", 'r', encoding = 'utf-8') as f:
     datastore = json.load(f)
 
 podcast = client.collection.get("Podcast")
@@ -46,19 +48,23 @@ text_splitter = RecursiveCharacterTextSplitter(
 
 transcripts_to_add = []
 
-for data in datastore:
-    chunked_transcript = text_splitter.create_documents([data["transcript"]])
-    for chunk in chunked_transcript:
-        transcripts_to_add.append(
-            wvc.DataObject(
-                properties={
-                    "title": data["title"],
-                    "transcript": chunk.page_content,
-                }
+#Chunking the data as the transcripts are too long and won't give good performance for semantic search
+with helper.std_out_err_redirect_tqdm() as orig_stdout:
+    for data in tqdm(datastore, desc='Importing transcripts', file = orig_stdout, unit = 'transcript'):
+        transcripts_to_add = []
+        chunked_transcript = text_splitter.create_documents([data["transcript"]])
+        for chunk in chunked_transcript:
+            transcripts_to_add.append(
+                wvc.DataObject(
+                    properties={
+                        "title": data["title"],
+                        "transcript": chunk.page_content,
+                    }
+                )
             )
-        )
-
-response = podcast.data.insert_many(transcripts_to_add)
-print(response.errors)
+        response = podcast.data.insert_many(transcripts_to_add)
+        message = str(data["title"]) + ' imported'
+        helper.log(message)
+        print(response.errors)
 
 print(podcast.query.fetch_objects(limit=2))
